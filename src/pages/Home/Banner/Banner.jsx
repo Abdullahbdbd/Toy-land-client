47880ccd8e48550be8cf034b166109be50d0c17b
Abdullahbdbd <abdullahmohammad262000@gmail.com>
2023-05-18 13:23:59 +0600

    Banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div></div>
+        <div className='max-w-screen-xl mx-auto'>
+            <div className="h-screen carousel carousel-vertical">
+                <div className="carousel-item h-full">
+                    <img className='w-screen' src="https://m.media-amazon.com/images/I/81WIgNnQCrL.jpg" />
+                </div>
+                <div className="carousel-item h-full">
+                    <img className='w-screen' src="https://images.immediate.co.uk/production/volatile/sites/28/2021/10/910_Of_The_Best_Green_Template-ff850e1.jpg?resize=768,574" />
+                </div>
+                <div className="carousel-item h-full">
+                    <img className='w-screen' src="https://cdn.shopify.com/s/files/1/1619/4221/products/DSC00516.jpg?v=1661735539&width=1445" />
+                </div>
+                
+            </div>
+        </div>
     );
 };
 
